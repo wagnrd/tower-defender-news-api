86895aae3720db03192db7e12b9a40a7bb15432e
wagnrd <projects@wagnrd.de>
2022-10-07 14:12:50 +0200

    adds cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import {publicArticleController} from "./controllers/article-controller";
 import {publicHealthCheckController} from "./controllers/heath-check-controller";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use("/health", publicHealthCheckController);
 app.use("/article", publicArticleController);
 
